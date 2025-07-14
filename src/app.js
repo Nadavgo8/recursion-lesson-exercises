acd4815e06caa91e38881bfee8fdf5bce2aecc9e
Nadavgo8 <19nadav99@gmail.com>
2025-07-14 21:48:55 +0300

    complete exercises

--- src/app.js
+++ src/app.js
@@ -4,25 +4,38 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
-
-}
+const findFactorial = function (num, product = 1) {
+  if (num === 1 || num === 2) {
+    return num * product;
+  }
+  return findFactorial(num - 1, product * num);
+};
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
-
-}
+const reverseString = function (rev, leftOfString) {
+  if (str === "") {
+    return "";
+  }
+  return reverseString(str.slice(1)) + str[0];
+};
 
 //Exercise 3
-const arr1 = [1, 2, 3]
-const arr2 = []
+const arr1 = [1, 2, 3];
+const arr2 = [];
 
-const swap = function() {
-  //Your code here
+const swap = function (arr1, arr2) {
+  if (arr1.length === 0) {
+    return;
+  }
+  const element = arr1.pop();
 
-}
+  swap(arr1, arr2);
+
+  arr2.push(element);
+};
+swap(arr1, arr2);
+console.log(arr1); //[]
+console.log(arr2); //[1, 2, 3]
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
-module.exports = { findFactorial, reverseString, swap }
+module.exports = { findFactorial, reverseString, swap };
